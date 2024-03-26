8d83bf11daf38184783818fb44bbb324501b9129
liuzhenghe <liuzhenghe@ikingtec.com>
2024-03-26 16:02:46 +0800

    feat: init

---
+++ examples/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <input
+      type="button"
+      value="click"
+      @click="handleClick"
+    >
+  </div>
+</template>
+
+<script>
+import { GetCesiumVersion } from '../packages/index'
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  mounted() {},
+  methods: {
+    handleClick() {
+      console.log('............GetCesiumVersion', GetCesiumVersion())
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  text-align: center;
+}
+</style>
---
+++ packages/index.js
@@ -0,0 +1,3 @@
+export function GetCesiumVersion() {
+  return Cesium.VERSION
+}
